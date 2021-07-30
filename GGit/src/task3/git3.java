9379dcf73f989f594e33aa1ec77ee7606573c74e
admin <admin@Roshani>
2021-07-30 20:22:55 +0530

    by shivansh

--- GGit/src/task3/git3.java
+++ GGit/src/task3/git3.java
@@ -4,8 +4,8 @@ public class git3
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("repository 3");
-		System.out.println(" pull repository 3");
+		System.out.println("repository 4");
+		System.out.println(" pull repository 4");
 	}
 
 }
---
+++ GGit/src/task4/git4.java
@@ -0,0 +1,11 @@
+package task4;
+
+public class git4 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("repository 3");
+		System.out.println(" pull repository 3");
+	}
+
+}
